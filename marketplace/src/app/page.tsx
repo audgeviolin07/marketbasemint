f4ff3abfec2f1d63216dc40e23c8858636583236
audgeviolin07 <audgeviolin07@gmail.com>
2024-05-18 06:45:24 -0400

    gave it a try

--- marketplace/src/app/page.tsx
+++ marketplace/src/app/page.tsx
@@ -1,21 +1,3 @@
-// import LandingPage from "@/components/Landing";
-// import type { Metadata } from "next";
-
-// export const metadata: Metadata = {
-//   title: "Basemint - Where E-sports Live",
-//   description: "Simple Marketplace",
-//   openGraph: {
-//     images: ['https://i.imgur.com/FjcUss9.png']
-//   }
-// };
-
-// export default function Home() {
-//   return (
-//     <main className="px-2 sm:px-8 md:px-24 py-12">
-//       <LandingPage />
-//     </main>
-//   );
-// }
 import LandingPage from "@/components/Landing";
 import type { Metadata } from "next";
 
@@ -23,7 +5,7 @@ export const metadata: Metadata = {
   title: "Basemint - Where E-sports Live",
   description: "Simple Marketplace",
   openGraph: {
-    images: ['https://i.imgur.com/FjcUss9.png']
+    images: ['https://bafkreiabag3ztnhe5pg7js4bj6sxuvkz3sdf76cjvcuqjoidvnfjz7vwrq.ipfs.dweb.link/']
   }
 };
 
