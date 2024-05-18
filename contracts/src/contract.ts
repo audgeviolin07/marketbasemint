80dc2c93fbb18646d585ef27aef1eddeaefd435c
Adam <adamomarali37@gmail.com>
2024-05-18 08:25:54 -0400

    hello world smart contract

---
+++ contracts/src/contract.ts
@@ -0,0 +1,19 @@
+// Find all our documentation at https://docs.near.org
+import { NearBindgen, near, call, view, UnorderedMap } from 'near-sdk-js';
+
+@NearBindgen({})
+class HelloNear {
+  greeting: string = 'Hello';
+  users: UnorderedMap<string> = new UnorderedMap<string>('users');
+
+  @view({}) // This method is read-only and can be called for free
+  get_greeting(): string {
+    return this.greeting;
+  }
+
+  @call({}) // This method changes the state, for which it cost gas
+  set_greeting({ greeting }: { greeting: string }): void {
+    near.log(`Saving greeting ${greeting}`);
+    this.greeting = greeting;
+  }
+}
