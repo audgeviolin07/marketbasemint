55cce8c1eb0c08ff55b533010140d31463c83676
danielh-hong <danielh.toronto@gmail.com>
2024-05-19 09:44:55 -0400

    Finished Players Page

--- marketplace/src/app/players/page.tsx
+++ marketplace/src/app/players/page.tsx
@@ -8,16 +8,10 @@ const Page = () => {
     '/mugi_card.png',
     '/SK_morten_card.png',
     '/surg_goblin_card.png',
-    // Add more images as needed
   ];
 
   return (
     <>
-      <style>
-        {`
-          @import url('https://fonts.googleapis.com/css2?family=Cinzel&display=swap');
-        `}
-      </style>
       <div style={{ 
         backgroundImage: `url('/background5.jpg')`, 
         backgroundSize: '120% 120%', 
@@ -30,18 +24,19 @@ const Page = () => {
       }}>
         <h1 style={{ 
           textAlign: 'center', 
-          fontSize: '2em', 
-          marginBottom: '40px', 
+          fontSize: '4em', 
+          marginBottom: '20px', 
           marginTop: '40px',
-          fontFamily: 'Cinzel, serif' ,
-          fontWeight: 'bold'
+          fontFamily: '"Arial Rounded MT Bold", Arial, sans-serif',
+          fontWeight: 'bold',
+          color: 'white'
         }}>Our Partnered Players</h1>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', maxWidth: '960px' }}>
-          {players.map((image, index) => (
-            <div key={index} style={{ border: '1px solid #ddd', borderRadius: '10px', padding: '20px', textAlign: 'center', transition: 'transform 0.3s ease', margin: '10px', width: '300px' }}>
-              <Image src={image} alt={`Player ${index + 1}`} width={300} height={300} />
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', maxWidth: '1400px' }}>
+        {players.map((image, index) => (
+            <div key={index} style={{ border: '1px solid #ddd', borderRadius: '10px', padding: '10px', textAlign: 'center', transition: 'transform 0.3s ease', margin: '10px', width: '350px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Image src={image} alt={`Player ${index + 1}`} width={300} height={300} />
             </div>
-          ))}
+        ))}
         </div>
       </div>
     </>
